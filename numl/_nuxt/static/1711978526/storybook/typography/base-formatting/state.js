1cf675c90781f0f63e5f7d4d2abbc20f80aabc08
tenphi <tenphi@users.noreply.github.com>
2024-04-01 13:35:58 +0000

    deploy: f27d2b540954a837255aec622a3f279ad6c1f557

---
+++ numl/_nuxt/static/1711978526/storybook/typography/base-formatting/state.js
@@ -0,0 +1,1 @@
+window.__NUXT__=(function(a){return {staticAssetsBase:"\u002F_nuxt\u002Fstatic\u002F1711978526",layout:"default",error:a,serverRendered:true,routePath:"\u002Fstorybook\u002Ftypography\u002Fbase-formatting",config:{_app:{basePath:"\u002F",assetsPath:"\u002F_nuxt\u002F",cdnURL:a},content:{dbHash:"e7270d6d"}}}}(null));
