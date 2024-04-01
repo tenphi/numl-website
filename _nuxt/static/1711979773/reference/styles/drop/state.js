70abb5dfa75b547678f2cfa74d91b8796abefb0b
tenphi <tenphi@users.noreply.github.com>
2024-04-01 14:21:23 +0000

    deploy: b2bf177a2cbf4b97f7860dcf7319d9b547e28212

--- _nuxt/static/1711979773/reference/styles/drop/state.js
+++
@@ -1,1 +0,0 @@
-window.__NUXT__=(function(a){return {staticAssetsBase:"\u002Fnuml-website\u002F_nuxt\u002Fstatic\u002F1711979773",layout:"default",error:a,serverRendered:true,routePath:"\u002Freference\u002Fstyles\u002Fdrop",config:{_app:{basePath:"\u002Fnuml-website\u002F",assetsPath:"\u002Fnuml-website\u002F_nuxt\u002F",cdnURL:a},content:{dbHash:"ee39f63e"}}}}(null));
